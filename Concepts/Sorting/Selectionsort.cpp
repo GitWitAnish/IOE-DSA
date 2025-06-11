11b641d58dc98b9915e808c566ad5cf36ec99562
GitWitAnish <anishkarki5710@gmail.com>
2025-06-11 18:07:24 +0545

    Selection sort

--- Concepts/Sorting/Selectionsort.cpp
+++ Concepts/Sorting/Selectionsort.cpp
@@ -1,40 +1,39 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-void bubblesort(int arr[], int n) {
-    // Bubble Sort Algorithm
-    for(int i = 0; i < n - 1; i++) {
-        for(int j = 0; j < n - i - 1; j++) {
-            if(arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j + 1]
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
+void printArray(int array[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    cout << array[i] << " ";
+  }
+  cout << endl;
 }
-
-int main() {
-    int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-
-    int arr[n];
-    cout << "Enter the elements: ";
-
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i];
+void swap(int *a, int *b)
+{
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+void selectionSort(int array[], int size)
+{
+  for (int i = 0; i < size - 1; i++)
+  {
+    int min = i;
+    for (int j = i; j < size; j++)
+    {
+      if (array[j] < array[min])
+      {
+        min = j;
+      }
     }
-
-    // Call the bubblesort function
-    bubblesort(arr, n);
-
-    cout << "Sorted array: ";
-    for(int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
-    return 0;
+    swap(&array[i], &array[min]);
+  }
+}
+int main()
+{
+  int data[] = {-10, 100, 12, 0, 12, 2, 1};
+  int size = sizeof(data) / sizeof(data[0]);
+  selectionSort(data, size);
+  cout << "Sorted array in ascending order: " << endl;
+  printArray(data, size);
 }
